1d71afbd26b23edb660ae3ac0804914926c0b9a1
冯攀峰 <fengpanfeng@corp.netease.com>
2018-07-14 00:59:57 +0800

    add:添加controller

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author 冯攀峰(fengpanfeng @ corp.netease.com)
  */
+@Controller
 public class LoginController {
     @RequestMapping("login")
     @ResponseBody
